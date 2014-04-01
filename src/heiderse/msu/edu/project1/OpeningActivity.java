24e15918289c150394f2b7dd01d26c30cc7a824c
jbull477 <jbull477@gmail.com>
2014-04-01 09:45:44 -0400

    Opening Activity Created

---
+++ src/heiderse/msu/edu/project1/OpeningActivity.java
@@ -0,0 +1,22 @@
+package heiderse.msu.edu.project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpeningActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		//setContentView(R.layout.);
+		}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+	}
+
+	
+	
+}

25548d15a6f010ecfd4f5740d93eb87052624e3f
jbull477 <jbull477@gmail.com>
2014-04-01 09:51:51 -0400

    Activity for new user created

--- src/heiderse/msu/edu/project1/OpeningActivity.java
+++ src/heiderse/msu/edu/project1/OpeningActivity.java
@@ -1,7 +1,9 @@
 package heiderse.msu.edu.project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OpeningActivity extends Activity {
 
@@ -15,6 +17,18 @@ public class OpeningActivity extends Activity {
 	protected void onSaveInstanceState(Bundle outState) {
 		// TODO Auto-generated method stub
 		super.onSaveInstanceState(outState);
+	}
+	
+	public void onNewUser(View view)
+	{
+		// Load the new user activity
+		Intent intent = new Intent(this, newUserActivity.class);
+		startActivity(intent);
+	}
+	
+	public void onLogin(View view)
+	{
+		
 	}
 
 	
---
+++ src/heiderse/msu/edu/project1/newUserActivity.java
@@ -0,0 +1,16 @@
+package heiderse.msu.edu.project1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class newUserActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle bundle) {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle);
+	}
+	
+	
+	
+}
